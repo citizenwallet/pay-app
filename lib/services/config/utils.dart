84994daa3d0aa90b1a1706c2dc5afa5ace18ee52
sajee_techi <sajeewa@techi.be>
2024-12-23 13:18:06 +0530

    config of service and theme

---
+++ lib/services/config/utils.dart
@@ -0,0 +1,17 @@
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+const Map<String, String> correctedAliases = {
+  'wallet': 'wallet.oak.community',
+  'oak': 'wallet.oak.community',
+  'usdc.polygon': 'app',
+};
+
+String fixLegacyAliases(String alias) {
+  if (correctedAliases.containsKey(alias)) {
+    return correctedAliases[alias]!;
+  }
+
+  final String defaultAlias = dotenv.get('DEFAULT_COMMUNITY_ALIAS');
+
+  return alias == 'localhost' || alias == '' ? defaultAlias : alias;
+}
