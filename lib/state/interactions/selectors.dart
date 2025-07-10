b352a0b79a4c755aa6e7d9b667cee6bd367a54c8
kev-techi <kevin@techi.be>
2025-07-10 08:45:39 +0200

    sort interactions

--- lib/state/interactions/selectors.dart
+++ lib/state/interactions/selectors.dart
@@ -1,3 +1,4 @@
+import 'package:collection/collection.dart';
 import 'package:pay_app/models/interaction.dart';
 import 'package:pay_app/state/interactions/interactions.dart';
 
@@ -6,5 +7,7 @@ List<Interaction> sortByUnreadAndDate(InteractionState state) {
       .where((interaction) => interaction.name
           .toLowerCase()
           .contains(state.searchQuery.toLowerCase()))
+      .sortedByCompare(
+          (interaction) => interaction.lastMessageAt, (a, b) => b.compareTo(a))
       .toList();
 }
